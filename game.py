4e60589287abe42da138241fbb82803c93aeabb9
Tom Swope <tjswope@gmail.com>
2024-03-15 09:34:48 -0400

    Initial commit

---
+++ game.py
@@ -0,0 +1,60 @@
+import pygame
+from constants import RED, WHITE
+from board import Board
+
+class Game:
+
+    def __init__(self, win):
+        self.board = Board()
+        self.turn = WHITE
+        self.win = win
+    
+    def update(self):
+        self.board.draw(self.win)
+        pygame.display.update()
+
+    def _init(self, first):
+      self.board = Board()
+
+      if first % 2 == 0:
+            self.turn = WHITE
+      else:
+            self.turn = RED
+
+      goes_first = "red" if self.turn == RED else "white"
+      print(goes_first + " goes first")
+
+
+    def winner(self, col):
+        return self.board.winner(col)
+
+    def reset(self, first):
+        self._init(first)
+
+    def add_piece(self, column):  
+      if self.board.add_piece(self.turn, column):
+        self.board.print()
+        if self.board.winner(self.turn):
+          winner = "red" if self.turn == RED else "white"
+          print(winner + " is the winner")
+        self.change_turn()
+
+
+    def change_turn(self):
+        if self.turn == RED:
+            self.turn = WHITE
+        else:
+            self.turn = RED
+
+    def get_board(self):
+      return self.board
+
+    # return the new board after a move.
+    # will pass a new board object to the game 
+    def ai_move(self,board):
+      self.board = board
+#      if self.board.winner(self.turn):
+#          winner = "red" if self.turn == RED else "white"
+#          print(winner + " is the winner")
+      self.change_turn()
+
---
+++ main.py
@@ -0,0 +1,48 @@
+import time
+import random as r
+import pygame
+from constants import WIDTH, HEIGHT, RED, WHITE, SQUARE_SIZE
+from game import Game
+from algorithm import minimax
+
+
+
+FPS = 60
+DEPTH = 5
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Connect 4')
+
+def get_col_from_mouse(pos):
+    x, y = pos
+    col = x // SQUARE_SIZE
+    return col
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    game = Game(WIN)
+
+    while run:
+
+        clock.tick(FPS)
+        # uncomment this if statement if you want to test your scoring algorithm without an AI.
+        if game.turn == WHITE:
+          value, new_board = minimax(game.get_board(), DEPTH, True)
+          game.ai_move(new_board)
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                col = get_col_from_mouse(pos)
+                game.add_piece(col)
+
+        game.update()
+
+    pygame.quit()
+
+
+main()
